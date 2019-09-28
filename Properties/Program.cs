cade832dd1efb4204cd5a8a7324b6fe0d5ddc9aa
Chris Roche <chris@wetrack.com>
2019-09-28 15:18:03 +0100

    Properties project added

---
+++ Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        public class Person
+        {
+            public DateTime BirthDate { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
