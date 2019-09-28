480f64a0476ba06ae52dd8ce9ba8401fce9fa60a
Chris Roche <chris@wetrack.com>
2019-09-28 17:08:14 +0100

    Inheritance constructors

---
+++ Constructors/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registration)
+            : base(registration)
+        {
+            Console.WriteLine("Car is being initialized. Reg no - " + registration);
+        }
+    }
+}
---
+++ Constructors/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Vehicle
+    {
+        private readonly string registration;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized");
+        //}
+
+        public Vehicle(string registration)
+        {
+            this.registration = registration;
+
+            Console.WriteLine("Vehicle initizlized - " + registration);
+        }
+    }
+}
