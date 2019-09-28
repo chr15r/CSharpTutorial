7f10a0f0a3452ca35310d39c72e0b49c385b0d0e
Chris Roche <chris@wetrack.com>
2019-09-28 19:15:37 +0100

    Method Overriding

---
+++ MethodOverriding/Shape.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodOverriding
+{
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw() // Overridden by child classes
+        {
+
+        }
+
+    }
+}
