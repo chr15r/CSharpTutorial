1df04863ab43facaded94e2e54e3fc1bb710123d
Chris Roche <chris@wetrack.com>
2019-09-28 14:20:15 +0100

    Error handling

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpTutorial
+{
+
+    partial class Program
+    {
+        public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                var sum = 0;
+                foreach (var i in numbers)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,14 +3,30 @@
 namespace CSharpTutorial
 {
 
-
     partial class Program
     {
 
-        static void Main(string[] args)  
+        static void Main(string[] args)
         {
-            //var p = Person.Parse("Mike");
-            //p.Introduce("John");
+            int number;
+            var results = int.TryParse("abc", out number);
+            if (results)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+                      
+
+
+        }
+
+        static void UsePerson()
+        {
+            var p = Person.Parse("Mike");
+            p.Introduce("John");
 
             var customer = new Customer(1, "Chris");
             var order = new Order();
@@ -18,8 +34,33 @@ namespace CSharpTutorial
 
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
-
-
         }
+
+        static void UseParams()
+        {
+            var calc = new Calculator();
+            calc.Add(1, 2, 3, 4, 5, 6, 7);
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                point.Move(null);
+                Console.WriteLine("Moved to ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Moved to ({0}, {1})", point.X, point.Y);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error - " + ex.Message);
+            }
+        }
+
     }
 }
