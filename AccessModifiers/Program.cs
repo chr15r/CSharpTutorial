deb458260ac1f554cf1b6d7b96e7df29c6c1d253
Chris Roche <chris@wetrack.com>
2019-09-28 16:50:43 +0100

    Access Modifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            //Amazon.RateCalculator cal = new RateCalculator(); // Now an internal class - can't access
+
+      
+        }
+    }
+}
---
+++ Amazon/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Amazon
+{
+
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            //var rating = CalculateRating(true);
+            //if(rating == 1)
+            //    Console.WriteLine("Promoted to level 1");
+            //else
+            //    Console.WriteLine("Promoted to level 2");
+
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
+            Console.WriteLine("Promote change");
+        }
+
+        //protected int CalculateRating(bool excludeOrders)
+        //{
+        //    return 0;
+        //}
+
+    }
+}
