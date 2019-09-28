2d474ac673d4168b85276297f2dc7276097a5e27
Chris Roche <chris@wetrack.com>
2019-09-28 15:45:40 +0100

    Indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+            Expiry = DateTime.Now.AddMinutes(10);
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+    }
+}
