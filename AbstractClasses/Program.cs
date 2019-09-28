28fe81e57d0a9e65d109382910dd3f4f092073dd
Chris Roche <chris@wetrack.com>
2019-09-28 19:33:46 +0100

    Abstract classes

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rect = new Rectangle();
+            rect.Draw();
+        }
+    }
+}
