80dae88395d5b6dd2d029bad843915a89a09de29
Chris Roche <chris@wetrack.com>
2019-09-29 13:41:16 +0100

    Interfaces/Testing

---
+++ Interfaces.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Interfaces.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Interfaces.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7deb175-1827-42e6-ba76-980cbe34dc28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interfaces.Tests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interfaces;
+
+namespace Interfaces.Tests
+{
+    [TestClass]
+    public class OrderProcessorTests
+    {
+
+        // METHODNAME_CONDITION_EXPECTATION
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Process_OrderIsAlreadyShipped_ThrowsAnException()
+        {
+            var orderProcessor = new OrderProcessor(new TestShippingCalculator());
+            var order = new Order
+            {
+                Shipment = new Shipment()
+
+            };
+
+            orderProcessor.Process(order);
+
+        }
+
+        [TestMethod]
+        public void Process_OrderIsNotShipped_ShouldSetTheShipmentPropertyOfTheOrder()
+        {
+            var orderProcessor = new OrderProcessor(new TestShippingCalculator());
+            var order = new Order();
+            orderProcessor.Process(order);
+
+            Assert.IsTrue(order.IsShipped);
+            Assert.AreEqual(1, order.Shipment.Cost);
+            Assert.AreEqual(DateTime.Today.AddDays(1), order.Shipment.ShippingDate);
+
+
+        }
+
+    }
+
+    public class TestShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1;
+        }
+    }
+}
